36598ea328daf4e43fac7ef4ca16202e693fbc3c
renfei147 <renfei8@live.cn>
2023-04-01 21:17:15 +0800

    加入日程表界面

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>学生日程管理系统</h1>
+  <component :is="currentView" />
 </template>
+
+<script lang="ts">
+import Home from './routes/Main.vue';
+import Login from './routes/Login.vue';
+import NotFound from './routes/NotFound.vue';
+const routes: any = {
+  '/': Home,
+  '/login': Login
+}
+export default {
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || '/'] || NotFound;
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+      this.currentPath = window.location.hash
+    })
+  }
+}
+</script>
---
+++ src/client/components/Calendar.vue
@@ -0,0 +1,33 @@
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
+
+<script lang="ts">
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import listPlugin from '@fullcalendar/list';
+import { CalendarOptions } from '@fullcalendar/core'
+import zhcnLocale from '@fullcalendar/core/locales/zh-cn'
+export default {
+  components: {
+    FullCalendar
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [timeGridPlugin, dayGridPlugin, listPlugin],
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay,listDay,listWeek'
+        },
+        locales: [zhcnLocale],
+        locale: 'zh-cn',
+        allDaySlot: false,
+      } as CalendarOptions
+    }
+  }
+}
+</script>
