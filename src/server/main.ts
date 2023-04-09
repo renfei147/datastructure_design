491f6fe088d9c6c7562b7772de37b2a2b11dd5ba
CengYuanhao <100276242+LinesHogan@users.noreply.github.com>
2023-04-09 22:26:11 +0800

    -debug HashTable.ts -add fs-extra

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,62 +1,30 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import { Schedule, User } from "../common/definitions";
+import { HashTable } from "./HashTable";
+import fse from "fs-extra";
+
 
 const app = express();
 
-const users: User[] = [
-  { name: '张三', id: '1' },
-  { name: '李四', id: '2' },
-  { name: '法伍', id: '3' },
-  { name: '可爱软萌的猫娘女仆yya', id: '4' }];
+let users: User[] ;
 
-const schedule: Schedule = {
-  firstDay: { year: 2023, month: 3, day: 3 },
-  courses: [
-    {
-      id: '1',
-      name: '喵喵叫小课堂',
-      startTime: 8,
-      duration: 3,
-      weekday: 2,
-      startWeek: 1,
-      endWeek: 10,
-      placeInfo: {
-        type: 'online',
-        link: 'https://example.com',
-      }
-    }
-  ],
-  activities: [
-    {
-      id: '1',
-      name: '练习喵喵叫',
-      startTime: 13,
-      repeat: {
-        type: 'weekly',
-        weekday: 4,
-        startWeek: 1,
-        endWeek: 10,
-      },
-      placeInfo: {
-        type: 'online',
-        link: 'https://example.com',
-      }
-    }
-  ],
-  tempworks: [
-    {
-      id: '1',
-      name: '一个临时事务',
-      day: { year: 2023, month: 3, day: 6 },
-      time: 10,
-      placeInfo: {
-        type: 'online',
-        link: 'https://example.com',
-      }
-    }
-  ]
-};
+let schedule: Schedule ;
+
+let myHashTable=new HashTable();
+myHashTable.put("shedule","my shedule");
+console.log(myHashTable.find("shedule"));
+
+// console.log(JSON.stringify(users));
+fse.readJSON("src/server/schedule.json").then((data)=>{
+  schedule=data;
+});
+
+fse.readJSON("src/server/users.json").then((data)=>{
+  users=data;
+});
+
+app.use(express.json())
 
 app.get("/api/users", (_, res) => {
   res.send(users);
