e472136ac1f1a3f827be465cc732112ebbceda59
CengYuanhao <100276242+LinesHogan@users.noreply.github.com>
2023-04-16 15:59:46 +0800

    完善了倒排列表

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,64 +1,58 @@
 import express from "express";
 import ViteExpress from "vite-express";
-import { Course, Schedule, User } from "../common/definitions";
+import { Activity, Course, Schedule, User,Tempwork } from "../common/definitions";
 import { HashTable } from "./HashTable";
 import fse from "fs-extra";
+
 let url = require("url");
 
-const isAtPresent = 1;
+const isAtPresent = 0;
 
 const app = express();
 
 let users: User[];
 
 let schedule: Schedule;
+
 let withStudentsCourse: {
   [key:string]:
   Course & { students: User[] }
 };
-// let withStudentsCourse: Map<string, Course & { students: User[] }>;
 
-let myHashTable = new HashTable();
-myHashTable.put("shedule", "my shedule");
-console.log(myHashTable.find("shedule"));
+let withStudentsActivity: {
+  [key:string]:
+  Activity&{students:User[]}
+};
 
+let withStudentsTempwork: {
+  [key:string]:
+  Tempwork&{students:User[]}
+};
 
-// console.log(JSON.stringify(users));
+// let myHashTable = new HashTable();
+// myHashTable.put("shedule", "my shedule");
+// console.log(myHashTable.find("shedule"));
+
 fse.readJSON("src/server/schedule.json").then((data) => {
   schedule = data;
 });
 
 fse.readJSON("src/server/users.json").then((data) => {
   users = data;
-  users.splice(4, 5);
+  if(isAtPresent) users.splice(4, 5);
 });
 
 fse.readJSON("src/server/withStudentCourse.json").then((data) => {
   withStudentsCourse = data;
-  // let a = withStudentsCourse.get('1');
 });
 
-//构造schedule
-function structSchedule(id: String): Schedule {
-  let mySchedule = JSON.parse(JSON.stringify(schedule));
-  for(const [key,value] of Object.entries(withStudentsCourse)){
-    for(const student of value.students){
-      if(student.id == id){
-        if(isAtPresent && value.name == "喵喵叫小课堂") continue;
-        mySchedule.courses.push(value);
-      }
-    }
-  }
-  // withStudentsCourse.forEach((course) => {
-  //   course.students.forEach((student) => {
-  //     if (student.id == id) {
-  //       if (isAtPresent && course.name == "喵喵叫小课堂") return;
-  //       mySchedule.courses.push(course);
-  //     }
-  //   });
-  // });
-  return mySchedule;
-}
+fse.readJSON("src/server/withStudentActivity.json").then((data) => {
+  withStudentsActivity = data;
+});
+
+fse.readJSON("src/server/withStudentTempwork.json").then((data) => {
+  withStudentsTempwork = data;
+});
 
 app.use(express.json())
 
@@ -67,7 +61,6 @@ app.get("/api/users", (_, res) => {
 });
 
 app.get("/api/schedule", (req, res) => {
-  // res.send(schedule);
   let params = url.parse(req.url, true).query;
   res.send(structSchedule(params.id));
 });
@@ -75,3 +68,34 @@ app.get("/api/schedule", (req, res) => {
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
 );
+
+
+//构造schedule
+function structSchedule(id: String): Schedule {
+  let mySchedule = JSON.parse(JSON.stringify(schedule));
+  for(const [key,value] of Object.entries(withStudentsCourse)){
+    for(const student of value.students){
+      if(student.id == id){
+        if(isAtPresent && value.name.includes("喵喵")) continue;
+        mySchedule.courses.push(value);
+      }
+    }
+  }
+  for(const [key,value] of Object.entries(withStudentsActivity)){
+    for(const student of value.students){
+      if(student.id == id){
+        if(isAtPresent && value.name.includes("喵喵")) continue;
+        mySchedule.activities.push(value);
+      }
+    }
+  }
+  for(const [key,value] of Object.entries(withStudentsTempwork)){
+    for(const student of value.students){
+      if(student.id == id){
+        if(isAtPresent && value.name.includes("喵喵")) continue;
+        mySchedule.tempworks.push(value);
+      }
+    }
+  }
+  return mySchedule;
+}
