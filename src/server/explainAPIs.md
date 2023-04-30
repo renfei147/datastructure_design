83045ebbeca8412a1c1632ef63d0dd87557c495a
CengYuanhao <100276242+LinesHogan@users.noreply.github.com>
2023-04-30 23:54:26 +0800

    完成了关于三种活动的增删改

--- src/server/explainAPIs.md
+++ src/server/explainAPIs.md
@@ -68,6 +68,47 @@ msg内容即为删除的course的id
 ```
 msg内容为需要更改的内容。更新时，唯一标识符是id。
 
+### 保存为json和显示信息
+在向后端提交post后，如果返回为“success”，则说明修改成功。这时候需要再次 ```get  /api/schedule``` 以获得新的schedule。
+
+在提交更新后，所有的更新都只是在内存中修改，下一次重启后端服务器将会清除这些更改。通过 ```get  /api/savechanges``` 可以将这些修改保存在以 ```com_``` 为首的几个文件中。这样做可以使你的修改在后端重启后继续保留。
+
+
+## 对activity和tempwork的介绍
+关于这部分的增删改功能，和course部分基本相同，不同在于msg字段中的内容是对应类型的内容。如对addtempwork，post的json为：
+```json
+{
+        "command":"add",
+        "msg":{
+            "name": "买冰淇淋",
+            "day": {
+                "year": 2023,
+                "month": 3,
+                "day": 4
+            },
+            "time": 17,
+            "placeInfo": {
+                "type": "online",
+                "link": "https://csrf.com"
+            },
+            "students": [
+                {
+                    "id": "1",
+                    "name": "张三"
+                },
+                {
+                    "id": "5",
+                    "name": "太阳能rf"
+                },
+                {
+                    "id": "4",
+                    "name": "可爱软萌的猫娘女仆yya"
+                }
+            ]
+        }
+    }
+```
+post这个json可以在临时事物中增加如msg字段描述的“买冰淇淋”事件
 
 ### 关于id字段的介绍
 在文件withStudentCourse.json中，每个键值对的格式例子为：
--- src/server/main.ts
+++ src/server/main.ts
@@ -17,29 +17,44 @@ let withStudentsCourse: {
   [key: string]:
   Course & { students: User[] } & { id: string }
 };
-let more_withStudentsCourse: {
-  [key: string]:
-  Course & { students: User[] }
-};
-
 let com_withStudentsCourse: {
   [key: string]:
   {
     command: "add" | "upd";
-    msg: Course & { students: User[] } ;
-    id: string
+    msg: Course & { students: User[] }&{id:string};
   } | {
     command: "del";
     msg: string
   }
 };
+
 let withStudentsActivity: {
   [key: string]:
   Activity & { students: User[] }
 };
+let com_withStudentsActivity: {
+  [key: string]:
+  {
+    command: "add" | "upd";
+    msg: Activity & { students: User[] }&{id:string};
+  } | {
+    command: "del";
+    msg: string
+  }
+};
+
 let withStudentsTempwork: {
   [key: string]:
   Tempwork & { students: User[] }
+}; let com_withStudentsTempwork: {
+  [key: string]:
+  {
+    command: "add" | "upd";
+    msg: Tempwork & { students: User[] };
+  } | {
+    command: "del";
+    msg: string
+  }
 };
 
 // let myHashTable = new HashTable();
@@ -52,6 +67,7 @@ let withStudentsTempwork: {
 // }
 // test(); 
 
+/*以下为读文件相关*/
 fse.readJSON("src/server/schedule.json").then((data) => {
   schedule = data;
 });
@@ -68,21 +84,43 @@ fse.readJSON("src/server/withStudentCourse.json").then((data) => {
     com_withStudentsCourse = data;
   }).then(() => {
     for (const [key, value] of Object.entries(com_withStudentsCourse)) {
-      if (value.command == "add") withStudentsCourse[value.id] = value.msg;
+      if (value.command == "add") withStudentsCourse[value.msg.id] = value.msg;
       if (value.command == "del") delete withStudentsCourse[value.msg];
-      if (value.command == "upd") withStudentsCourse[value.id] = value.msg;
+      if (value.command == "upd") withStudentsCourse[value.msg.id] = value.msg;
     }
   });
 });
 
 fse.readJSON("src/server/withStudentActivity.json").then((data) => {
   withStudentsActivity = data;
+}).then(() => {
+  fse.readJSON("src/server/com_withStudentActivity.json").then((data) => {
+    com_withStudentsActivity = data;
+  }).then(() => {
+    for (const [key, value] of Object.entries(com_withStudentsActivity)) {
+      if (value.command == "add") withStudentsActivity[value.msg.id] = value.msg;
+      if (value.command == "del") delete withStudentsActivity[value.msg];
+      if (value.command == "upd") withStudentsActivity[value.msg.id] = value.msg;
+    }
+  });
 });
 
 fse.readJSON("src/server/withStudentTempwork.json").then((data) => {
   withStudentsTempwork = data;
+}).then(() => {
+  fse.readJSON("src/server/com_withStudentTempwork.json").then((data) => {
+    com_withStudentsTempwork = data;
+  }).then(() => {
+    for (const [key, value] of Object.entries(com_withStudentsTempwork)) {
+      if (value.command == "add") withStudentsTempwork[value.msg.id] = value.msg;
+      if (value.command == "del") delete withStudentsTempwork[value.msg];
+      if (value.command == "upd") withStudentsTempwork[value.msg.id] = value.msg;
+    }
+  });
 });
+/*以上为读文件相关*/
 
+/*以下为基础操作*/
 app.use(express.json())
 
 app.get("/api/users", (_, res) => {
@@ -90,6 +128,7 @@ app.get("/api/users", (_, res) => {
 });
 
 app.get("/api/schedule", (req, res) => {
+  console.log(withStudentsActivity);
   let params = url.parse(req.url, true).query;
   res.send(structSchedule(params.id));
 });
@@ -102,7 +141,10 @@ app.get("/api/shortestPath", (req, res) => {
     res.send(data);
   })
 });
+/*以上为基础操作*/
 
+
+/*以下为course增删改操作*/
 app.post("/api/addcourse", (req, res) => {
   let body = req.body;
   let course = body.msg;
@@ -114,13 +156,12 @@ app.post("/api/addcourse", (req, res) => {
   nextID++;
   console.log("nextID: " + nextID.toString());
   console.log(course);
+  course["id"]=nextID.toString();
   com_withStudentsCourse[Object.keys(com_withStudentsCourse).length + 1] = {
     command: "add",
-    msg: course,
-    id: nextID.toString()
+    msg: course
   };
   withStudentsCourse[nextID.toString()] = course;
-  fse.writeJSON("src/server/com_withStudentCourse.json", com_withStudentsCourse);
   res.send("success");
 });
 
@@ -131,7 +172,6 @@ app.post("/api/delcourse", (req, res) => {
     delete withStudentsCourse[body.msg];
     com_withStudentsCourse[Object.keys(com_withStudentsCourse).length + 1] = body;
     result = true;
-    fse.writeJSON("src/server/com_withStudentCourse.json", com_withStudentsCourse);
   }
   if (result) {
     res.send("success");
@@ -143,12 +183,128 @@ app.post("/api/delcourse", (req, res) => {
 app.post("/api/updcourse", (req, res) => {
   let body = req.body;
   let course = body.msg;
-  withStudentsCourse[body.msg.id] = course;
-  com_withStudentsCourse[Object.keys(com_withStudentsCourse).length + 1] = body;
-  fse.writeJSON("src/server/com_withStudentCourse.json", com_withStudentsCourse);
+  if (body.id in withStudentsCourse) {
+    withStudentsCourse[body.id] = course;
+    com_withStudentsCourse[Object.keys(com_withStudentsCourse).length + 1] = body;
+    res.send("success");
+  } else {
+    res.status(401).send("not exist");
+  }
+});
+/*以上为course增删改操作*/
+
+
+/*以下为activity增删改操作*/
+app.post("/api/addactivity", (req, res) => {
+  let body = req.body;
+  let course = body.msg;
+  let nextID = 0;
+  for (const [key, value] of Object.entries(withStudentsActivity)) {
+    if (parseInt(key) > nextID) nextID = parseInt(key);
+    if (value.name == course.name) return res.status(401).send("already exist");
+  }
+  nextID++;
+  console.log("nextID: " + nextID.toString());
+  console.log(course);
+  course["id"]=nextID.toString();
+  com_withStudentsActivity[Object.keys(com_withStudentsActivity).length + 1] = {
+    command: "add",
+    msg: course
+  };
+  withStudentsActivity[nextID.toString()] = course;
   res.send("success");
 });
 
+app.post("/api/delactivity", (req, res) => {
+  let body = req.body;
+  let result = false;
+  if (body.msg in withStudentsActivity) {
+    delete withStudentsActivity[body.msg];
+    com_withStudentsActivity[Object.keys(com_withStudentsActivity).length + 1] = body;
+    result = true;
+  }
+  if (result) {
+    res.send("success");
+  } else {
+    res.status(401).send("not exist");
+  }
+});
+
+app.post("/api/updactivity", (req, res) => {
+  let body = req.body;
+  let course = body.msg;
+  if (body.id in withStudentsActivity) {
+    withStudentsActivity[body.id] = course;
+    com_withStudentsActivity[Object.keys(com_withStudentsActivity).length + 1] = body;
+    res.send("success");
+  } else {
+    res.status(401).send("not exist");
+  }
+
+});
+/*以上为activity增删改操作*/
+
+
+/*以下为tempwork增删改操作*/
+app.post("/api/addtempwork", (req, res) => {
+  let body = req.body;
+  let course = body.msg;
+  let nextID = 0;
+  for (const [key, value] of Object.entries(withStudentsTempwork)) {
+    if (parseInt(key) > nextID) nextID = parseInt(key);
+    if (value.name == course.name) return res.status(401).send("already exist");
+  }
+  nextID++;
+  console.log("nextID: " + nextID.toString());
+  console.log(course);
+  course["id"]=nextID.toString();
+  com_withStudentsTempwork[Object.keys(com_withStudentsTempwork).length + 1] = {
+    command: "add",
+    msg: course
+  };
+  withStudentsTempwork[nextID.toString()] = course;
+  res.send("success");
+});
+
+app.post("/api/deltempwork", (req, res) => {
+  let body = req.body;
+  let result = false;
+  if (body.msg in withStudentsTempwork) {
+    delete withStudentsTempwork[body.msg];
+    com_withStudentsTempwork[Object.keys(com_withStudentsTempwork).length + 1] = body;
+    result = true;
+  }
+  if (result) {
+    res.send("success");
+  } else {
+    res.status(401).send("not exist");
+  }
+});
+
+app.post("/api/updtempwork", (req, res) => {
+  let body = req.body;
+  let course = body.msg;
+  if (body.id in withStudentsTempwork) {
+    withStudentsTempwork[body.id] = course;
+    com_withStudentsTempwork[Object.keys(com_withStudentsTempwork).length + 1] = body;
+    res.send("success");
+  } else {
+    res.status(401).send("not exist");
+  }
+
+});
+/*以上为tempwork增删改操作*/
+
+app.get("/api/savechanges", (req, res) => {
+  // console.log(com_withStudentsActivity);
+  Promise.all([
+    fse.writeJSON("src/server/com_withStudentCourse.json", com_withStudentsCourse),
+    fse.writeJSON("src/server/com_withStudentActivity.json", com_withStudentsActivity),
+    fse.writeJSON("src/server/com_withStudentTempwork.json", com_withStudentsTempwork)
+  ]).then(()=>{
+      res.send("success");
+    })
+})
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
